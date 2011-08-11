e2a24c366fb1aa5d83a359aa0386f4e9927f2083
Michael Weingärtner <mialwe@googlemail.com>
2011-08-12 00:11:33 +0200

    tweaked governor values again

--- drivers/cpufreq/cpufreq_conservative.c
+++ drivers/cpufreq/cpufreq_conservative.c
@@ -29,7 +29,7 @@
  * It helps to keep variable names smaller, simpler
  */
 
-#define DEF_FREQUENCY_UP_THRESHOLD		(65)
+#define DEF_FREQUENCY_UP_THRESHOLD		(60)
 #define DEF_FREQUENCY_DOWN_THRESHOLD		(45)
 
 /*
@@ -95,7 +95,7 @@ static struct dbs_tuners {
 	.down_threshold = DEF_FREQUENCY_DOWN_THRESHOLD,
 	.sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR,
 	.ignore_nice = 0,
-	.freq_step = 15,
+	.freq_step = 17,
 };
 
 static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
